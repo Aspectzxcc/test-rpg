bd16f5903fd4b23e868815db777c90a3a211c701
Aspectzxcc <josh10nathan@gmail.com>
2024-07-02 05:37:57 +0800

    added a melee weapon subclass

---
+++ game/weapons/MeleeWeapon.py
@@ -0,0 +1,12 @@
+from game.weapons.Weapon import Weapon
+
+class MeleeWeapon(Weapon):
+    def __init__(self, damage, cooldown, range):
+        super().__init__(damage, cooldown)
+        self.range = range
+
+    def use(self, current_time, *args, **kwargs):
+        # Implement melee-specific use behavior here
+        super().use(current_time, *args, **kwargs)
+        print("Melee weapon used at range:", self.range)
+        # Additional melee-specific logic
